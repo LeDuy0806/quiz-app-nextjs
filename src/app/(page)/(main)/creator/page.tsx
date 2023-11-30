f0eede78bd00a00a93d4033e1db2f28efbab2ca3
Long Tran <plongtran239@gmail.com>
2023-11-30 15:08:35 +0700

    Update Creator UI and refactor package (#33)

--- src/app/(page)/(main)/creator/page.tsx
+++ src/app/(page)/(main)/creator/page.tsx
@@ -4,15 +4,12 @@ import ContentEditor from 'src/components/Creator/ContentEditor';
 import CreatorNavbar from 'src/components/Creator/CreatorNavbar';
 import CreatorSidebar from 'src/components/Creator/CreatorSidebar';
 
-import QuestionSettingSidebar from 'src/components/Creator/QuestionSettingSidebar';
-
 function CreatorPage() {
     return (
         <main>
             <CreatorNavbar />
             <CreatorSidebar />
             <ContentEditor />
-            <QuestionSettingSidebar />
         </main>
     );
 }
